928d9566aac925a7579c40204cf4b7597837e1da
Paul Wijhenke <paul@paulsplace.nl>
2017-07-16 11:39:53 +0200

    Update to new DBWrkr test structure

--- test/dbwrkr-mongodb-tests.js
+++ test/dbwrkr-mongodb-tests.js
@@ -1,15 +1,9 @@
 /* eslint no-console: 0 */
 const DBWrkrMongoDb = require('../dbwrkr-mongodb');
-const dbWrkrTests = require('dbwrkr').tests;
+const tests = require('dbwrkr').tests;
 
-
-const testOptions = {
+tests({
   storage: new DBWrkrMongoDb({
     dbName: 'dbwrkr_tests'
   })
-};
-
-
-dbWrkrTests(testOptions, function (err) {
-  if (err) throw err;
 });
